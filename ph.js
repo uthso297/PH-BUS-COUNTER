f70363903e889d42e97ed7b8ae8c81cab6c9e601
uthso297 <uthso119464@gmail.com>
2024-09-22 02:12:44 +0600

    completed purchase section

--- ph.js
+++ ph.js
@@ -1,1 +1,30 @@
-// I am empty now..i will work soon...
+// const grandTotal = document.getElementById('grand-total').innerText;
+// let grandTotalNumber = parseFloat(grandTotal);
+
+// const new15Discount = document.getElementById('new-15').innerText;
+// let new15DiscountNumber = parseFloat(new15Discount);
+
+// const couple20Discount = document.getElementById('couple-20').innerText;
+// let couple20DiscountNumber = parseFloat(couple20Discount);
+
+// const new15 = document.getElementById('new-15-text').innerText;
+
+// const couple20 = document.getElementById('couple-20-text').innerText;
+
+
+// document.getElementById('apply').addEventListener('click',function(){
+//     console.log('applied');
+
+//     const discount = document.getElementById('discount-input').value;
+//     console.log(discount);
+
+//     if(discount === new15){
+//         grandTotalNumber = totalTakaNumber - (totalTakaNumber*(new15DiscountNumber/100));
+//         document.getElementById('grand-total').innerText = grandTotalNumber;
+//     }
+//     else if(discount === couple20){
+//         grandTotalNumber = totalTakaNumber - (totalTakaNumber*(couple20DiscountNumber/100));
+//         document.getElementById('grand-total').innerText = grandTotalNumber;
+//     }
+
+// })
--- ph1.js
+++ ph1.js
@@ -14,6 +14,9 @@ const ticketPriceNumber = parseFloat(ticketPrice);
 const totalTaka = document.getElementById('total-taka').innerText;
 let totalTakaNumber = parseFloat(totalTaka);
 
+
+// discount
+
 const grandTotal = document.getElementById('grand-total').innerText;
 let grandTotalNumber = parseFloat(grandTotal);
 
@@ -23,8 +26,12 @@ let new15DiscountNumber = parseFloat(new15Discount);
 const couple20Discount = document.getElementById('couple-20').innerText;
 let couple20DiscountNumber = parseFloat(couple20Discount);
 
-// const discountInput = document.getElementById('discount-input').value;
-// console.log(discountInput);
+const new15 = document.getElementById('new-15-text').innerText;
+
+const couple20 = document.getElementById('couple-20-text').innerText;
+
+
+
 
 
 for (let button of buttons) {
@@ -52,7 +59,27 @@ for (let button of buttons) {
             totalTakaNumber += ticketPriceNumber;
             document.getElementById('total-taka').innerText = totalTakaNumber;
 
-            
+
+            // discount
+
+
+            document.getElementById('apply').addEventListener('click', function () {
+                console.log('applied');
+
+                const discount = document.getElementById('discount-input').value;
+                console.log(discount);
+
+                if (discount === new15) {
+                    grandTotalNumber = totalTakaNumber - (totalTakaNumber * (new15DiscountNumber / 100));
+                    document.getElementById('grand-total').innerText = grandTotalNumber;
+                }
+                else if (discount === couple20) {
+                    grandTotalNumber = totalTakaNumber - (totalTakaNumber * (couple20DiscountNumber / 100));
+                    document.getElementById('grand-total').innerText = grandTotalNumber;
+                }
+
+            })
+
 
         }
 
